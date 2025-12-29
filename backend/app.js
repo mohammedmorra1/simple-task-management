7751799df49fdc572625426b1ffee2e03b9c0aab
Mohammed Morra <wolfeyplays123@gmail.com>
2025-12-29 07:52:31 +0200

    db setup - created users and tasks tables - connected node to db

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,11 @@ import express from "express";
 import cors from "cors";
 import sqlite3 from "sqlite3";
 import bcrypt from "bcryptjs";
+
+//connect to database
+const db = new sqlite3.Database("database.db", (err) => {
+  if (err) {
+    console.log(err.message);
+  }
+  console.log("Connected to the database!");
+});
